b7ba5db642d2bc71563d1f5fa57cff38a15def16
Peter Morris <mrpmorris@gmail.com>
2020-01-01 19:09:43 +0000

    Naming conventions

--- samples/01-SimpleMemberValidationSample/SimpleMemberValidationSample/ValidationProfiles/PersonValidation.cs
+++ samples/01-SimpleMemberValidationSample/SimpleMemberValidationSample/ValidationProfiles/PersonValidation.cs
@@ -11,11 +11,11 @@ namespace SimpleMemberValidationSample.ValidationProfiles
 				.ForMember(c => c.HomeAddress, v => v.NotNull())
 				.ForMember(c => c.DateOfDeath, v => v.NotNull())
 				.ForMember(c => c.Name, v => v.MinLength(3).MaxLength(4))
-				.WhenAll(c => c.Name, m => m.NotNull(), v => v
+				.WhenAll(c => c.Name, its => its.NotNull(), then => then
 					.ForMember(c => c.DateOfDeath, v => v.NotNull())
 					.ForMember(c => c.HomeAddress.Lines, v => v.MinLength(2))
 				)
-				.WhenAll(c => c.DateOfDeath, m => m.NotNull(), v => v
+				.WhenAll(c => c.DateOfDeath, its => its.NotNull(), then => then
 					.ForMember(c => c.DateOfBirth, v => v.NotLessThan(c => c.DateOfDeath))
 					.ForMember(c => c.Name, v => v.EqualTo(c => c.HomeAddress.Country.Code))
 				)
