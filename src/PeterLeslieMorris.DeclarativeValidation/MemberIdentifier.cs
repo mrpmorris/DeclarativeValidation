392ae6f62f25c0323c755cc7f74fe8189746ba7f
Peter Morris <mrpmorris@gmail.com>
2020-02-13 15:22:47 +0000

    Search suppliers

---
+++ src/PeterLeslieMorris.DeclarativeValidation/MemberIdentifier.cs
@@ -0,0 +1,16 @@
+﻿namespace PeterLeslieMorris.DeclarativeValidation
+{
+	public class MemberIdentifier
+	{
+		public object Owner { get; set; }
+		public string MemberName { get; set; }
+
+		public MemberIdentifier() { }
+
+		public MemberIdentifier(object owner, string memberName)
+		{
+			Owner = owner;
+			MemberName = memberName;
+		}
+	}
+}
---
+++ src/PeterLeslieMorris.DeclarativeValidation/Validators/IValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PeterLeslieMorris.DeclarativeValidation.RuleFactories
+{
+	public interface IValidator
+	{
+		Task ValidateAsync(IServiceProvider serviceProvider, object value);
+	}
+
+	public interface IValidator<T>
+	{
+		Task ValidateAsync(IServiceProvider serviceProvider, T value);
+	}
+}
